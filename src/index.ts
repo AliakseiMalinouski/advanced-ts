36b105801b955b7c4d04aca555b7a5cd03ac4f91
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-06-16 16:08:20 +0200

    class by T

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
-const n: number = 1;
-const heh = 'heh'
+class Factory<T> {
+
+    list: T[] = [];
+
+    push(value: T) {
+        this.list.push(value);
+    }
+
+    pop() {
+        return this.list.pop();
+    }
+
+    find(index: T) {
+        return this.list.find((_, idx) => idx === index);
+    }
+
+}
+
+const factory = new Factory<number>();
+
+factory.pop();
+factory.push(999);
+const result = factory.find(1);
